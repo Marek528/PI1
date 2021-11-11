e921eb9539a4ee93253d1da8cc83595d0ed1a2be
z21ite06 <z21ite06@z00p3-05.SPSKNM>
2021-11-11 14:43:06 +0100

    Update pokrocily.py

--- hodina/pokrocily.py
+++ hodina/pokrocily.py
@@ -1,3 +1,8 @@
 while(True):
-    x = input(int("zadaj cislo: "))
-    if x ==
+    x = int(input("zadaj cislo: "))
+    if x == (1,2,3,4,5,6,7,8,9):
+        continue
+    else:
+        break
+
+print(x)
