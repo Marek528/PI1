1c20f801baf15c38428d491b3423f254bdfe9204
z21ite06 <z21ite06@z00p3-05.SPSKNM>
2021-11-11 13:26:30 +0100

    Create zoznamy.py

---
+++ hodina/zoznamy.py
@@ -0,0 +1,38 @@
+zoznam_prazdny = []
+zoznam_cisel = [1,5,8,9,15]
+zoznam_pismen = ['a','t','r']
+zoznam_mix = ["slovo",14,'a','!', 55]
+
+zoznam_cisel[0] = 99999
+print(zoznam_cisel[4])
+print(zoznam_pismen[2])
+print(zoznam_mix[-1])
+
+print("---------------")
+print(zoznam_pismen)
+print(zoznam_mix)
+print(zoznam_cisel)
+
+
+zoznam = list()
+print(zoznam)
+zoznam_range = list(range(3, 7))
+print(zoznam_range)
+
+#orezavanie
+neorezany_zoznam = list(range(10))
+print(neorezany_zoznam)
+
+print(neorezany_zoznam[0:5])
+print(neorezany_zoznam[2:8])
+print(neorezany_zoznam[1:7:2])
+print(neorezany_zoznam[2:9:3])
+
+
+#velkost/ kolko prvkov obsahuje zoznam
+x = [5,8,1,3,"slovo"]
+print(len(x))
+
+#prechadzanie zoznamu
+
+
