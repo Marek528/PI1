c65e260ad03a1b6ac2a9135e8748241a54de1002
Marek528 <91258444+Marek528@users.noreply.github.com>
2021-11-15 18:30:27 +0100

    Update zoznamy.py

--- hodina/zoznamy.py
+++ hodina/zoznamy.py
@@ -12,12 +12,13 @@ print("---------------")
 print(zoznam_pismen)
 print(zoznam_mix)
 print(zoznam_cisel)
-
+print("---------------")
 
 zoznam = list()
 print(zoznam)
 zoznam_range = list(range(3, 7))
 print(zoznam_range)
+print("---------------")
 
 #orezavanie
 neorezany_zoznam = list(range(10))
@@ -28,10 +29,13 @@ print(neorezany_zoznam[2:8])
 print(neorezany_zoznam[1:7:2])
 print(neorezany_zoznam[2:9:3])
 
+print("---------------")
 
 #velkost/ kolko prvkov obsahuje zoznam
 x = [5,8,1,3,"slovo"]
 print(len(x))
+
+print("---------------")
 
 #prechadzanie zoznamu
 zoznam_prvkov = ["jablko", "hruska", "banan"]
@@ -39,10 +43,13 @@ zoznam_prvkov = ["jablko", "hruska", "banan"]
 for prvok in zoznam_prvkov:
     print(prvok)
 
+print("---------------")
+
 #2.
 for index in range(len(zoznam_prvkov)):
     print(zoznam_prvkov[index])
 
+print("---------------")
 
 #metody zoznam
 mylist = [1,5,8,55,500]
@@ -53,13 +60,14 @@ mylist.append(1)
 mylist.append(0)
 print(mylist)
 
+print("---------------")
+
 #pop
 hodnota = mylist.pop()
 print(mylist)
 print(hodnota)
 
-
-
+print("---------------")
 
 #funkcie
 #len
@@ -67,8 +75,13 @@ print(hodnota)
 mylist2 = [1,54,7,2,74,-10]
 print("minimum", min(mylist2))
 print("maximum", max(mylist2))
+
+print("---------------")
+
 #suma / spocita prvky zoznamu
 print("suma", sum(mylist2))
+
+print("---------------")
 
 print(mylist2)
 #otoci list naopak
