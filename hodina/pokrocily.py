0b54204115a5a3742f2cffec221e397428b1eb78
Marek528 <91258444+Marek528@users.noreply.github.com>
2021-11-15 15:52:59 +0100

    Update pokrocily.py

--- hodina/pokrocily.py
+++ hodina/pokrocily.py
@@ -1,7 +1,11 @@
+cisla = []
+
 while(True):
     premenna = input("zadaj cislo: ")
     if premenna != "":
         premenna = int(premenna)
+        cisla.append(premenna)
     else:
         break
 
+print(cisla)
