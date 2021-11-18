70e222727b5627b2f7eb5f3d63bc349fa951b583
Marek528 <91258444+Marek528@users.noreply.github.com>
2021-11-18 17:12:29 +0100

    Update main.py

--- 6hodina/main.py
+++ 6hodina/main.py
@@ -1,8 +1,6 @@
 """
 domy = [4, 2, 0, 5, 0, 1, 5, 4]
 
-i = 0
-
 for i in domy:
     neobyvane_domy = domy.count(0)
     max_pocet = domy.count(max(domy))
@@ -16,8 +14,6 @@ print('pocet maximalnych domov je', max_pocet)
 
 
 domy = [1,5,0,8,9,2,4,5,3,7,4,1,2,5,0]
-
-i = 0
 
 for i in domy:
     neobyvane_domy = domy.count(0)
