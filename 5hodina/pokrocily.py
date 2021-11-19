52c1c27b9d2a585aa2deed935e2991129f68c5e0
Marek528 <91258444+Marek528@users.noreply.github.com>
2021-11-19 18:36:56 +0100

    Update pokrocily.py

--- 5hodina/pokrocily.py
+++ 5hodina/pokrocily.py
@@ -11,10 +11,13 @@ while(True):
 zoradene_cisla = sorted(cisla)
 dlzka_cisel = len(zoradene_cisla)
 vypocet_medianu = int(dlzka_cisel / 2)
+median = zoradene_cisla[vypocet_medianu]
+
 
 for i in range(dlzka_cisel):
+    x = cisla[i]
+    rozdiel_od_medianu = x - median
+    print(x, 'sa od medianu odlisuje o ',rozdiel_od_medianu)
     
-
-
 
 
