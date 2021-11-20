68bb6e38a3b6f7f3e89d6783a520313bef208312
Marek528 <91258444+Marek528@users.noreply.github.com>
2021-11-20 17:35:09 +0100

    update

--- 6hodina/main.py
+++ 6hodina/main.py
@@ -15,12 +15,7 @@ print('pocet maximalnych domov je', max_pocet)
 
 domy = [1,5,0,8,9,2,4,5,3,7,9,1,9,5,0]
 
-for i in domy:
-    neobyvane_domy = domy.count(0)
-    max_pocet = domy.count(max(domy))
-    
-
 print('pocet obyvatelov je', sum(domy))
-print('neobyvanych domov je', neobyvane_domy)
+print('neobyvanych domov je', domy.count(0))
 print('maximalny pocet obyvatelov v dome je', max(domy))
-print('pocet maximalnych domov je', max_pocet)
+print('pocet maximalnych domov je', domy.count(max(domy)))
