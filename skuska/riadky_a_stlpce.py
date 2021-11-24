9806a6a30e61052deaccddaafbed1899de97f3fb
Marek528 <91258444+Marek528@users.noreply.github.com>
2021-11-24 18:14:14 +0100

    Update riadky_a_stlpce.py

--- skuska/riadky_a_stlpce.py
+++ skuska/riadky_a_stlpce.py
@@ -1,3 +1,4 @@
+"""
 riadky= int(input('zadaj pocet riadkov: '))
 stlpce = int(input('zadaj pocet stlpcov: '))
 
@@ -5,3 +6,8 @@ for i in range(0, stlpce):
     for j in range(0, riadky):
         print('*', end="")
     print()
+"""
+
+string = "Informatika s Mišom"
+print(string[5:8] + 'e' + string[5:8] + string[8:12])
+
