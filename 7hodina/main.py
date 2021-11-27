2cdf23df95d5323e6b783e18b027ebb6b469115d
Marek528 <91258444+Marek528@users.noreply.github.com>
2021-11-27 14:53:12 +0100

    Update main.py

--- 7hodina/main.py
+++ 7hodina/main.py
@@ -1,17 +1,13 @@
 pocet_suborov = int(input('zadaj pocet suborov: '))
 list = []
 
-with open ('./basnicka.txt', encoding='UTF-8') as subor:
+with open('./basnicka.txt', encoding='UTF-8') as subor:
     for riadok in subor:
         list += riadok.split()
 
 dlzka_basne = len(list)
 x = 0
-
-
-"""
 for i in range(pocet_suborov):
     if i >= dlzka_basne:
         x = i - dlzka_basne
-    open('./7hodina/%s.txt' % i, 'w', encoding='UTF-8').write(list[x])
-"""
+    open('./%s.txt' % i, 'w', encoding='UTF-8').write(list[x])
