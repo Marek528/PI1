c76b2d6ab2aceca4a8df5968413f8400eca94f3a
Marek528 <91258444+Marek528@users.noreply.github.com>
2021-11-27 14:43:23 +0100

    Update main.py

--- 7hodina/main.py
+++ 7hodina/main.py
@@ -7,7 +7,11 @@ with open ('./7hodina/basnicka.txt', encoding='UTF-8') as subor:
 
 dlzka_basne = len(list)
 x = 0
+
+
+"""
 for i in range(pocet_suborov):
     if i >= dlzka_basne:
         x = i - dlzka_basne
     open('./7hodina/%s.txt' % i, 'w', encoding='UTF-8').write(list[x])
+"""
