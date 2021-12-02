c3beb92ddd9dc201b24c74f0c65b719294070ee3
Marek528 <91258444+Marek528@users.noreply.github.com>
2021-12-02 12:50:49 +0100

    update

---
+++ python.py
@@ -0,0 +1,10 @@
+from random import randint
+
+privlastky = ['maly', 'velky', 'pekny', 'skaredy', 'chudy', 'vesely', 'smutny', 'nudny', 'zaujimavy', 'tucny' ]
+zoznam_mien = []
+
+meno = input('zadaj meno: ')
+priezvisko = input('zadaj priezvisko: ')
+mismas = meno + '' + privlastky[random.randint(0,9)] + '' + priezvisko
+zoznam_mien.append(mismas)
+print(zoznam_mien)
