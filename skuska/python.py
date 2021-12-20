25b273c5684faed421052d6c5a10ae75e4d9a74a
Marek528 <golias.marek.06@gmail.com>
2021-12-20 20:58:25 +0100

    Update python.py

--- skuska/python.py
+++ skuska/python.py
@@ -1,40 +1,8 @@
-"""
-for riadok in range(1,5):
-    for stlpec in range(1, riadok):
-        print(stlpec)
-
-1
-2
-3
-4
-
-1-2 = 1
-1-3 = 1 2
-1-4 = 1 2 3
-
-1
-1
-2
-1
-2
-3
-"""
-
-"""
-if __name__ == '__main__':
-    n = int(input())
-    arr = map(int, input().split())
-    
-zoradene = sorted(arr)
-premenna = max(zoradene)
-
-for i in zoradene[::-1]:
-    if i == premenna:
-        continue
-    print(i)
-    break
-"""
+dictionary = {
+	"name" : "Marek",
+	"surname" : "Golias"
+}
 
 
-
-
+dictionary["name"] = "Meno"
+print(dictionary)
